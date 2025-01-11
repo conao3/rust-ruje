7767b58e27c3c188344510a0a765bac07b5c87e6
conao3 <conao3@gmail.com>
2025-01-11 22:49:32 +0900

    use nix rust

--- default.nix
+++ default.nix
@@ -1,5 +1,12 @@
-{ rustPlatform }:
+{ makeRustPlatform, rust-bin }:
 
+let
+  toolchain = rust-bin.stable.latest.default;
+  rustPlatform = makeRustPlatform {
+    cargo = toolchain;
+    rustc = toolchain;
+  };
+in
 rustPlatform.buildRustPackage {
   pname = "ruje";
   version = "0.1.0";
