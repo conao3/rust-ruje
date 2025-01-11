da777d9697c1e640761ff0ce677f21a8499dc426
conao3 <conao3@gmail.com>
2025-01-11 23:37:42 +0900

    add REPL

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+fn repl() {
+    println!("Welcome to the Ruje!");
+    loop {
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        println!("{}", input.trim());
+    }
+}
+
 fn main() {
     println!("Hello, world!");
+    repl();
 }
