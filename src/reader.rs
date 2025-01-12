4ec5ccbad392d0a9cdaf6ed7acffc820b7fb80f4
conao3 <conao3@gmail.com>
2025-01-12 10:17:13 +0900

    peak char

--- src/reader.rs
+++ src/reader.rs
@@ -1,4 +1,5 @@
 use crate::types::{RujeAtom, RujeExp};
+use anyhow::anyhow;
 use anyhow::Result;
 
 use regex::Regex;
@@ -23,25 +24,25 @@ impl Reader<'_> {
         self.input = self.input.trim_start();
     }
 
-    fn read_atom(&mut self) -> Result<RujeAtom> {
+    fn read_atom(&mut self) -> Result<RujeExp> {
         self.skip_whitespace();
 
         if let Some(m) = INT_PATTERN.find(self.input) {
             let value = m.as_str().parse::<i64>().unwrap();
             self.input = &self.input[m.end()..];
-            return Ok(RujeAtom::Int(value));
+            return Ok(RujeAtom::Int(value).into());
         }
 
         if let Some(m) = FLOAT_PATTERN.find(self.input) {
             let value = m.as_str().parse::<f64>().unwrap();
             self.input = &self.input[m.end()..];
-            return Ok(RujeAtom::Float(value));
+            return Ok(RujeAtom::Float(value).into());
         }
 
         if let Some(m) = SYMBOL_PATTERN.find(self.input) {
             let value = m.as_str().to_string();
             self.input = &self.input[m.end()..];
-            return Ok(RujeAtom::Symbol(value));
+            return Ok(RujeAtom::Symbol(value).into());
         }
 
         Err(anyhow::anyhow!("Invalid input"))
@@ -49,7 +50,16 @@ impl Reader<'_> {
 
     pub fn read(&mut self) -> Result<RujeExp> {
         self.skip_whitespace();
-        Ok(self.read_atom()?.into())
+
+        let c = self
+            .input
+            .chars()
+            .next()
+            .ok_or(anyhow!("Unexpected end of input"))?;
+
+        match c {
+            _ => self.read_atom()
+        }
     }
 }
 
@@ -62,14 +72,14 @@ mod tests {
     fn test_read_atom() {
         let mut reader = Reader::new("123");
         let atom = reader.read_atom().unwrap();
-        assert_eq!(atom, RujeAtom::Int(123));
+        assert_eq!(atom, RujeAtom::Int(123).into());
 
         let mut reader = Reader::new("123.456");
         let atom = reader.read_atom().unwrap();
-        assert_eq!(atom, RujeAtom::Float(123.456));
+        assert_eq!(atom, RujeAtom::Float(123.456).into());
 
         let mut reader = Reader::new("abc");
         let atom = reader.read_atom().unwrap();
-        assert_eq!(atom, RujeAtom::Symbol("abc".to_string()));
+        assert_eq!(atom, RujeAtom::Symbol("abc".to_string()).into());
     }
 }
